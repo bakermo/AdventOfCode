fac7255c01dd322456f35fd27b6877ecd5f39f84
bakermo <matthew.baker4175@gmail.com>
2024-12-04 02:31:54 -0500

    option for either pure 2d array or doofy jagged array that I was using

--- Helpers.cs
+++ Helpers.cs
@@ -21,12 +21,27 @@ namespace AdventOfCode
             return new string(input.Reverse().ToArray());
         }
 
-        public static char [][] GetGrid(string[] input)
+        public static char [][] GetJaggedGrid(string[] input)
         {
             var grid = new char[input.Length][];
             for (int i = 0; i < input.Length; i++)
             {
                 grid[i] = input[i].ToCharArray();
+            }
+            return grid;
+        }
+
+        public static char[,] GetGrid(string[] input)
+        {
+            int rows = input.Length;
+            int columns = input[0].Length;
+            var grid = new char[rows, columns];
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < columns; col++)
+                {
+                    grid[row, col] = input[row][col];
+                }
             }
             return grid;
         }
@@ -52,29 +67,62 @@ namespace AdventOfCode
             return input.ToList();
         }
 
-        public static List<string> GetDiagonals(char[][] grid, int diagonalLength)
+        public static List<string> GetDiagonals(char[][] jaggedGrid, int diagonalLength)
         {
             int diagonalSafetyBound = diagonalLength - 1;
             var diagonals = new List<string>();
-            for (int i = 0; i < grid.Length; i++)
+            for (int i = 0; i < jaggedGrid.Length; i++)
             {
-                for (int j = 0; j < grid[i].Length; j++)
+                for (int j = 0; j < jaggedGrid[i].Length; j++)
                 {
-                    if (i + diagonalSafetyBound < grid.Length && j + diagonalSafetyBound < grid[i].Length)
+                    if (i + diagonalSafetyBound < jaggedGrid.Length && j + diagonalSafetyBound < jaggedGrid[i].Length)
                     {
                         var diagonal = string.Empty;
                         for (int k = 0; k < diagonalLength; k++)
                         {
-                            diagonal += grid[i + k][j + k];
+                            diagonal += jaggedGrid[i + k][j + k];
                         }
                         diagonals.Add(diagonal);
                     }
-                    if (i + diagonalSafetyBound < grid.Length && j - diagonalSafetyBound >= 0)
+                    if (i + diagonalSafetyBound < jaggedGrid.Length && j - diagonalSafetyBound >= 0)
                     {
                         var diagonal = string.Empty;
                         for (int k = 0; k < diagonalLength; k++)
                         {
-                            diagonal += grid[i + k][j - k];
+                            diagonal += jaggedGrid[i + k][j - k];
+                        }
+                        diagonals.Add(diagonal);
+                    }
+                }
+            }
+            return diagonals;
+        }
+
+        public static List<string> GetDiagonals(char[,] grid, int diagonalLength)
+        {
+            int diagonalSafetyBound = diagonalLength - 1;
+            var diagonals = new List<string>();
+            int rows = grid.GetLength(0);
+            int columns = grid.GetLength(1);    
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < columns; col++)
+                {
+                    if (row + diagonalSafetyBound < rows && col + diagonalSafetyBound < columns)
+                    {
+                        var diagonal = string.Empty;
+                        for (int k = 0; k < diagonalLength; k++)
+                        {
+                            diagonal += grid[row + k, col + k];
+                        }
+                        diagonals.Add(diagonal);
+                    }
+                    if (row + diagonalSafetyBound < rows && col - diagonalSafetyBound >= 0)
+                    {
+                        var diagonal = string.Empty;
+                        for (int k = 0; k < diagonalLength; k++)
+                        {
+                            diagonal += grid[row + k, col - k];
                         }
                         diagonals.Add(diagonal);
                     }
@@ -84,15 +132,15 @@ namespace AdventOfCode
         }
 
 
-        public static List<Cross> GetCenteredCrosses(char[][] grid, int crossLength)
+        public static List<Cross> GetCenteredCrosses(char[][] jaggedGrid, int crossLength)
         {
             int safetyBound = crossLength / 2;
             var crosses = new List<Cross>();
-            for (int i = 0; i < grid.Length; i++)
+            for (int i = 0; i < jaggedGrid.Length; i++)
             {
-                for (int j = 0; j < grid[i].Length; j++)
+                for (int j = 0; j < jaggedGrid[i].Length; j++)
                 {
-                    if (i + safetyBound < grid.Length && j + safetyBound < grid[i].Length && i - safetyBound >= 0 && j - safetyBound >= 0)
+                    if (i + safetyBound < jaggedGrid.Length && j + safetyBound < jaggedGrid[i].Length && i - safetyBound >= 0 && j - safetyBound >= 0)
                     {
                         var cross = new Cross()
                         {
@@ -102,8 +150,40 @@ namespace AdventOfCode
 
                         for (int k = -safetyBound; k <= safetyBound; k++)
                         {
-                            cross.TopToBottom += grid[i + k][j + k];
-                            cross.BottomToTop += grid[i - k][j + k];
+                            cross.TopToBottom += jaggedGrid[i + k][j + k];
+                            cross.BottomToTop += jaggedGrid[i - k][j + k];
+                        }
+                        crosses.Add(cross);
+                    }
+                }
+            }
+            return crosses;
+        }
+
+        public static List<Cross> GetCenteredCrosses(char[,] grid, int crossLength)
+        {
+            int safetyBound = crossLength / 2;
+            var crosses = new List<Cross>();
+
+            int rows = grid.GetLength(0);
+            int columns = grid.GetLength(1);
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < columns; col++)
+                {
+                    if (row + safetyBound < rows && col + safetyBound < columns && row - safetyBound >= 0 && col - safetyBound >= 0)
+                    {
+                        var cross = new Cross()
+                        {
+                            Row = row,
+                            Column = col
+                        };
+
+                        for (int k = -safetyBound; k <= safetyBound; k++)
+                        {
+                            cross.TopToBottom += grid[row + k, col + k];
+                            cross.BottomToTop += grid[row - k, col + k];
                         }
                         crosses.Add(cross);
                     }
