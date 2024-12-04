c817a11e3d4a5ba74c90137a2782d6e22c4094eb
bakermo <matthew.baker4175@gmail.com>
2024-12-03 20:11:12 -0500

    command line args for speed

--- Program.cs
+++ Program.cs
@@ -1,55 +1,61 @@
 ﻿using AdventOfCode;
 using System.Reflection;
 
-RunMode mode = RunMode.Both;
-int year = 2024;
-int day = 3;
-bool useTest = false;
-
-string fileName = useTest ? $"samples/{year}/Day{day}.txt" : $"inputs/{year}/Day{day}.txt";
-
-var input = File.ReadAllLines(fileName);
-
-IDaySolver solver = CreateDayInstance(day);
-
-if (mode == RunMode.Both || mode == RunMode.Part1)
+class Program
 {
-    string run = $"{year} Day {day} Part 1";
-    Console.WriteLine($"Running {run}...");
-    var solution = solver.Part1(input);
-    Console.WriteLine($"{run} Solution: {solution}");
-    Console.WriteLine();
-}
-
-if (mode == RunMode.Both || mode == RunMode.Part2)
-{
-    string run = $"{year} Day {day} Part 2";
-    Console.WriteLine($"Running {run}...");
-    var solution = solver.Part2(input);
-    Console.WriteLine($"{run} Solution: {solution}");
-    Console.WriteLine();
-}
-
-//Console.ReadLine();
-
-IDaySolver CreateDayInstance(int day)
-{
-    string className = $"Day{day}";
-    Type dayType = Assembly.GetExecutingAssembly()
-        .GetTypes()
-        .FirstOrDefault(t => t.Name == className && typeof(IDaySolver).IsAssignableFrom(t));
-
-    if (dayType != null)
+    static void Main(string[] args)
     {
-        return (IDaySolver)Activator.CreateInstance(dayType);
+        RunMode mode = RunMode.Both;
+        int year = 2024;
+        int day = 4;
+        bool useTest = args.Contains("-t");
+
+        string fileName = useTest ? $"samples/{year}/Day{day}.txt" : $"inputs/{year}/Day{day}.txt";
+
+        var input = File.ReadAllLines(fileName);
+
+        IDaySolver solver = CreateDayInstance(day);
+        var sample = useTest ? "SAMPLE " : string.Empty; ;
+        if (mode == RunMode.Both || mode == RunMode.Part1)
+        {
+            string run = $"{year} Day {day} Part 1 {sample}";
+            Console.WriteLine($"Running {run}...");
+            var solution = solver.Part1(input);
+            Console.WriteLine($"{run} Solution: {solution}");
+            Console.WriteLine();
+        }
+
+        if (mode == RunMode.Both || mode == RunMode.Part2)
+        {
+            string run = $"{year} Day {day} Part 2 {sample}";
+            Console.WriteLine($"Running {run}...");
+            var solution = solver.Part2(input);
+            Console.WriteLine($"{run} Solution: {solution}");
+            Console.WriteLine();
+        }
+
+        //Console.ReadLine();
     }
 
-    return null;
-}
+    static IDaySolver CreateDayInstance(int day)
+    {
+        string className = $"Day{day}";
+        Type dayType = Assembly.GetExecutingAssembly()
+            .GetTypes()
+            .FirstOrDefault(t => t.Name == className && typeof(IDaySolver).IsAssignableFrom(t));
 
-enum RunMode
-{
-    Part1,
-    Part2,
-    Both
+        if (dayType != null)
+        {
+            return (IDaySolver)Activator.CreateInstance(dayType);
+        }
+
+        return null;
+    }
+
+    enum RunMode
+    {
+        Part1,
+        Part2,
+        Both
+    }
 }
