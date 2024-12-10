be87deee01ee5e1fea7a575024e8fe779ccb6298
bakermo <matthew.baker4175@gmail.com>
2024-12-10 00:53:22 -0500

    Day 10 part 2 LFG!!!

--- 2024/Day10.cs
+++ 2024/Day10.cs
@@ -14,10 +14,10 @@ namespace AdventOfCode._2024
             grid.PrintGrid();
 
             var startPositions = GetStartPositions(grid);
-            foreach (var position in startPositions)
-            {
-                Console.WriteLine("Row: {0}, Col: {1}", position.Row, position.Column);
-            }
+            //foreach (var position in startPositions)
+            //{
+            //    Console.WriteLine("Row: {0}, Col: {1}", position.Row, position.Column);
+            //}
 
             var count = 0;
 
@@ -38,7 +38,31 @@ namespace AdventOfCode._2024
 
         public string Part2(string[] input)
         {
-            return input[0].Length.ToString();
+            var grid = input.GetGrid();
+            grid.PrintGrid();
+
+            var startPositions = GetStartPositions(grid);
+            //foreach (var position in startPositions)
+            //{
+            //    Console.WriteLine("Row: {0}, Col: {1}", position.Row, position.Column);
+            //}
+
+            var count = 0;
+
+            foreach (var position in startPositions)
+            {
+                var foundNines = new Dictionary<Position, Dictionary<Position, int>>();
+                TraverseV2(grid, position, position, foundNines);
+                foreach (var startingPosition in foundNines)
+                {
+                    var found = startingPosition.Value;
+                    count += found.Sum(x => x.Value);
+                }
+            }
+
+
+
+            return count.ToString();
         }
 
         private List<Position> GetStartPositions(char[,] grid)
@@ -82,7 +106,7 @@ namespace AdventOfCode._2024
                     {
                         if (nextValue == 9)
                         {
-                            Console.WriteLine("Row: {0}, Col: {1}", position.Row, position.Column);
+                            //Console.WriteLine("Row: {0}, Col: {1}", position.Row, position.Column);
                             if (foundNines.ContainsKey(originalStartingPosition))
                             {
                                 foundNines[originalStartingPosition].Add(position);
@@ -98,6 +122,60 @@ namespace AdventOfCode._2024
                         else
                         {
                             Traverse(grid, position, originalStartingPosition, foundNines);
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private void TraverseV2(char[,] grid, Position startPosition, Position originalStartingPosition,
+            Dictionary<Position, Dictionary<Position, int>> foundNines)
+        {
+            var value = int.Parse(grid[startPosition.Row, startPosition.Column].ToString());
+            var testPositions = new List<Position>(value);
+            testPositions.Add(startPosition.GetNextPosition(Direction.Up));
+            testPositions.Add(startPosition.GetNextPosition(Direction.Down));
+            testPositions.Add(startPosition.GetNextPosition(Direction.Left));
+            testPositions.Add(startPosition.GetNextPosition(Direction.Right));
+
+            foreach (var position in testPositions)
+            {
+                if (grid.IsInBounds(position.Row, position.Column))
+                {
+                    var isValid = int.TryParse(grid[position.Row, position.Column].ToString(), out int nextValue);
+                    if (!isValid)
+                    {
+                        continue;
+                    }
+                    if (nextValue == value + 1)
+                    {
+                        if (nextValue == 9)
+                        {
+                            //Console.WriteLine("Row: {0}, Col: {1}", position.Row, position.Column);
+                            if (foundNines.ContainsKey(originalStartingPosition))
+                            {
+                                var found = foundNines[originalStartingPosition];
+                                if (found.ContainsKey(position))
+                                {
+                                    found[position]++;
+                                }
+                                else
+                                {
+                                    found.Add(position, 1);
+                                }
+                            }
+                            else
+                            {
+                                var positionsFound = new Dictionary<Position, int>();
+                                positionsFound.Add(position, 1);
+                                foundNines.Add(originalStartingPosition, positionsFound);
+                            }
+                            //return;
+                        }
+                        else
+                        {
+                            TraverseV2(grid, position, originalStartingPosition, foundNines);
                         }
 
                     }
