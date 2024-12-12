ddeb0da652ca453a5fc4c850ffe81d6d86820489
bakermo <matthew.baker4175@gmail.com>
2024-12-11 19:39:57 -0500

    prep day 12

---
+++ 2024/Day12.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2024
+{
+    internal class Day12 : IDaySolver
+    {
+        public string Part1(string[] input)
+        {
+            return input[0].Length.ToString();
+        }
+
+        public string Part2(string[] input)
+        {
+            return input[0].Length.ToString();
+        }
+    }
+}
