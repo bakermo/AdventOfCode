e9dc71f4db736b54acb71cf039a975bad96df0fe
Matt Baker <matthew.baker4175@gmail.com>
2024-12-22 11:12:46 -0500

    day 16 part 1. not sure why my original implementation didnt work. really need to refactor this into a general solution. also, equality operator suddenly doesn't work on Position class. what a mess.

--- 2024/Day16.cs
+++ 2024/Day16.cs
@@ -31,37 +31,44 @@ namespace AdventOfCode._2024
             var path = new Dictionary<Position, Position>();
 
             Direction currentDirection = Direction.Right;
-            var unvisited = new HashSet<Position>();
-            unvisited.Add(startNode.Position);
-            unvisited.Add(endPosition.Position);
 
             var validNodes = grid.GetPositionsByValue('.')
                 .ToDictionary(x => x, x => (Previous: (Position)null, Distance: int.MaxValue));
+
+            var seen = new HashSet<(Position, Direction)>();
 
             validNodes.Add(startNode.Position, (null, 0));
             validNodes.Add(endPosition.Position, (null, int.MaxValue));
 
             var minQueue = new PriorityQueue<Node, int>();
-            //minQueue.Enqueue(startNode.Position, startNode.Distance);
-            //minQueue.Enqueue(endPosition.Position, endPosition.Distance);
-
-            //foreach (var node in  nodes)
-            //{
-            //    unvisited.Add(node);
-            //    minQueue.Enqueue(node, int.MaxValue);
-            //}
 
             minQueue.Enqueue(startNode, startNode.Weight);
             while (minQueue.Count > 0)
             {
-                var current = minQueue.Dequeue();
+                //var current = minQueue.Dequeue();
+                minQueue.TryDequeue(out var current, out var distance);
 
+                seen.Add((current.Position, current.Direction));
+                Console.WriteLine("current position: " + current.Position);
+                if (current.Position.Row == endPosition.Position.Row && current.Position.Column == endPosition.Position.Column)
+                {
+                    //TODO: why the fuck doesn't this respect equality operator?
+                    Console.WriteLine("distance: " + distance);
+                    break;
+                }
+                //if (current.Position == endPosition.Position)
+                //{
+                //    Console.WriteLine("distance: ", distance);
+                //    break;
+                //}
                 // I think I need to figure out what the previous one was from here
                 var nodes = new List<Node>();
                 nodes.Add(new Node()
                 {
                     Position = current.Position.GetNextPosition(current.Direction),
-                    Weight = 1,
+                    Weight = current.Weight + 1,
+
+                    //Weight = 1,
                     Direction = current.Direction
                 });
 
@@ -82,14 +89,17 @@ namespace AdventOfCode._2024
                 nodes.Add(new Node()
                 {
                     Position = current.Position.GetNextPosition(current.Direction.TurnLeft()),
-                    Weight = 1000,
+                    Weight = current.Weight + 1001,
+                    //Weight = 1001,
                     Direction = current.Direction.TurnLeft()
                 });
 
                 nodes.Add(new Node()
                 {
                     Position = current.Position.GetNextPosition(current.Direction.TurnRight()),
-                    Weight = 1000,
+                    Weight = current.Weight + 1001,
+
+                    //Weight = 1001,
                     Direction = current.Direction.TurnRight()
                 });
 
@@ -99,50 +109,74 @@ namespace AdventOfCode._2024
                 {
                     if (validNodes.ContainsKey(node.Position))
                     {
-                        var distance = validNodes[node.Position].Distance;
-
-                        var cost = currentNodeDistance + node.Weight;
-
-                        if (cost < distance)
+                        if (seen.Contains((node.Position, node.Direction)))
                         {
-                            validNodes[node.Position] = (current.Position, cost);
-                            //node.Weight = cost;
-                            //if (path.ContainsKey(current.Position))
-                            //{
-                            //    path[current.Position] = node.Position;
-                            //}
-                            //else
-                            //{
-                            //    path.Add(current.Position, node.Position);
-                            //}
-
-                            minQueue.Remove(node, out _, out _);
-                            minQueue.Enqueue(node, cost);
+                            continue;
                         }
+
+                        //var newCost = current.Weight + node.Weight;
+                        //node.Weight = newCost;
+                        minQueue.Remove(node, out _, out _);
+                        minQueue.Enqueue(node, node.Weight);
+
+                        //var distance = validNodes[node.Position].Distance;
+
+                        //var cost = currentNodeDistance + node.Weight;
+
+                        //validNodes[node.Position] = (current.Position, cost);
+                        ////node.Weight = cost;
+                        ////if (path.ContainsKey(current.Position))
+                        ////{
+                        ////    path[current.Position] = node.Position;
+                        ////}
+                        ////else
+                        ////{
+                        ////    path.Add(current.Position, node.Position);
+                        ////}
+
+                        //minQueue.Remove(node, out _, out _);
+                        //minQueue.Enqueue(node, cost);
+
+                        //if (cost < distance)
+                        //{
+                        //    validNodes[node.Position] = (current.Position, cost);
+                        //    //node.Weight = cost;
+                        //    //if (path.ContainsKey(current.Position))
+                        //    //{
+                        //    //    path[current.Position] = node.Position;
+                        //    //}
+                        //    //else
+                        //    //{
+                        //    //    path.Add(current.Position, node.Position);
+                        //    //}
+
+                        //    minQueue.Remove(node, out _, out _);
+                        //    minQueue.Enqueue(node, cost);
+                        //}
                     }
                 }
 
             }
 
             var currentNode = endPosition.Position;
-            while (currentNode != null)
-            {
-                grid[currentNode.Row, currentNode.Column] = 'X';
-                var distance = validNodes[currentNode].Distance;
-                Console.WriteLine($"({currentNode.Row} {currentNode.Column}) {currentNode} {distance}");
-                currentNode = validNodes[currentNode].Previous;
-                //if (path.ContainsKey(currentNode))
-                //{
-                //    var next = path[currentNode];
-                //    grid[currentNode.Row, currentNode.Column] = 'X';
-                //    currentNode = next;
-                //}
-                //else
-                //{
-                //    currentNode = null;
-                //}
+            //while (currentNode != null && currentNode != startNode.Position)
+            //{
+            //    grid[currentNode.Row, currentNode.Column] = 'X';
+            //    var distance = validNodes[currentNode].Distance;
+            //    Console.WriteLine($"({currentNode.Row} {currentNode.Column}) {currentNode} {distance}");
+            //    currentNode = validNodes[currentNode].Previous;
+            //    //if (path.ContainsKey(currentNode))
+            //    //{
+            //    //    var next = path[currentNode];
+            //    //    grid[currentNode.Row, currentNode.Column] = 'X';
+            //    //    currentNode = next;
+            //    //}
+            //    //else
+            //    //{
+            //    //    currentNode = null;
+            //    //}
 
-            }
+            //}
 
             //foreach (var node in path)
             //{
@@ -153,6 +187,11 @@ namespace AdventOfCode._2024
 
 
             var result = validNodes[endPosition.Position].Distance;
+            // 7029 vs 7036 expected, 7 off
+            // 11037 vs 11048 expected, 11 off
+            // 101395 too low!
+            // 101496/101495 too high!
+            // 101391 incorrect (duh because 101395 is too low
             return result.ToString();
         }
 
